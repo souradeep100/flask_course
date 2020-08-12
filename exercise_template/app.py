7ae43e255bf673230d2caed499ac4883719e8a72
souracha <souradch@gmail.com>
2020-08-13 01:37:19 +0530

    form and session practice

--- exercise_template/app.py
+++ exercise_template/app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField, IntegerField, Form
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app.config['SECRET_KEY'] = 'mysecretkey'
 
+class MyForm(Form):
+    mail1 = StringField('mail',_name="mail",id='mail')
+    submit = SubmitField('submit now')
+@app.route('/',methods=['POST','GET'])
+def index():
+    form = MyForm(request.form)
+    if request.method == 'POST'  and 'mail' not in session:
+        print("soura")
+        email = form.mail1.data
+        msg = f"the mail id is {email} "
+        flash(msg)
+        session['mail'] =  email
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form)
+'''
 @app.route('/result')
 def result():
     email = request.args.get("email")
     password = request.args.get("password")
     return render_template('result.html',email=email)
+'''
 if __name__ == "__main__":
      app.run(debug=True)
